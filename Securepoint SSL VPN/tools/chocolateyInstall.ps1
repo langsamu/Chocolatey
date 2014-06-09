b1cda51d8c85ea97a694a3cbc659d8f434b995a3
Samu Lang <langsamu@hotmail.com>
2014-06-09 10:28:40 +0100

    v1.0.3

--- Securepoint SSL VPN/tools/chocolateyInstall.ps1
+++ Securepoint SSL VPN/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'securepointsslvpn' 'exe' '' 'http://downloads.sourceforge.net/project/securepoint/SecurepointSSLVPN_%20v1.exe?r=&ts=1391552203'
+Install-ChocolateyPackage 'securepointsslvpn' 'exe' '' 'http://downloads.sourceforge.net/project/securepoint/SecurepointSSLVPN_%20v1.0.3.exe?r=&ts=1391552203'
