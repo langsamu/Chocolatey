c33a1dbfcc520ee195c20636a973ae4721c73c6e
Samu Lang <langsamu@users.noreply.github.com>
2020-05-17 18:11:08 +0100

    Adds checksum, improves structure

--- Last.fm Scrobbler/tools/chocolateyInstall.ps1
+++ Last.fm Scrobbler/tools/chocolateyInstall.ps1
@@ -1,1 +1,10 @@
-Install-ChocolateyPackage 'lastfmscrobbler' 'msi' '/passive' 'https://cdn.last.fm/client/Win/LastFM_Scrobbler_3.1.29.msi'
+$packageArgs = @{
+  packageName   = 'lastfmscrobbler'
+  fileType      = 'msi'
+  silentArgs    = "/passive"
+  url           = 'https://cdn.last.fm/client/Win/LastFM_Scrobbler_3.1.29.msi'
+  checksum      = 'DF00345A5D09C7598541B19F20709C5B2CA531762304184ED89C2F1A0A28A8CA'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
